d65614e01ff8ae266bb7bc7f7b0a8f95043d481b
LuluAllende19 <abigail_lucii@hotmail.com>
2020-05-20 09:48:44 -0300

    Agregando app empleado

---
+++ AppRestaurantEmpleado/app/src/main/java/com/example/appempleado/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.appempleado
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
